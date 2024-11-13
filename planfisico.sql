41429b3c9d929fec43b2d093951a91adba0366db
aarontimana04 <112721918+aarontimana04@users.noreply.github.com>
2024-11-13 17:21:41 -0500

    Update planfisico.sql

--- planfisico.sql
+++ planfisico.sql
@@ -1,34 +1,34 @@
 CREATE DATABASE CONTRATOS_DIGITALES;
-
+-- 
 CREATE TABLE Personas (
   dni CHAR(8) PRIMARY KEY NOT NULL,
   correo VARCHAR(25) NOT NULL,
-  Telefono INT NOT NULL,
+  telefono INT NOT NULL,
   nombre_persona VARCHAR(25) NOT NULL,
-  primerapellido VARCHAR(15) NOT NULL,
-  segundoapellido VARCHAR(15) NOT NULL
+  primer_apellido VARCHAR(15) NOT NULL,
+  segundo_apellido VARCHAR(15) NOT NULL
 );
-
+--
 CREATE TABLE Empresas (
   id_empresa INT PRIMARY KEY IDENTITY(1,1),
   ruc CHAR(11) NOT NULL,
   nombre_empresa VARCHAR(15) NOT NULL
 );
-
+--
 CREATE TABLE Rol (
   id_rol INT PRIMARY KEY IDENTITY(1,1),
-  nombre_rol VARCHAR(10) NOT NULL,
+  nombre_rol VARCHAR(15) NOT NULL,
   detalles VARCHAR(50) NOT NULL
 );
-
+-- 
 CREATE TABLE Clientes (
   dni CHAR(8) NOT NULL,
   direccion VARCHAR(25) NOT NULL,
-  fechanacimiento DATE NOT NULL,
+  fecha_nacimiento DATE NOT NULL,
   FOREIGN KEY (dni) REFERENCES Personas(dni),
   PRIMARY KEY (dni)
 );
-
+-- 
 CREATE TABLE Usuarios (
   dni CHAR(8) NOT NULL,
   id_empresa INT NOT NULL,
@@ -38,17 +38,17 @@ CREATE TABLE Usuarios (
   FOREIGN KEY (id_rol) REFERENCES Rol (id_rol),
   PRIMARY KEY (dni) 
 );
-
+--
 CREATE TABLE Estados (
   id_estado INT PRIMARY KEY IDENTITY(1,1),
-  nombre_estado VARCHAR(10) NOT NULL
+  nombre_estado VARCHAR(15) NOT NULL
 );
-
+--
 CREATE TABLE Grupos (
   id_grupo INT PRIMARY KEY IDENTITY(1,1),
   nombre_grupo VARCHAR(15) NOT NULL
 );
-
+-- 
 CREATE TABLE ClienteGrupo (
   dni CHAR(8) NOT NULL,
   id_grupo INT NOT NULL,
@@ -57,7 +57,7 @@ CREATE TABLE ClienteGrupo (
   FOREIGN KEY (id_grupo) REFERENCES Grupos (id_grupo),
   PRIMARY KEY (dni, id_grupo)
 );
-
+-- 
 CREATE TABLE Contratos (
   id_contrato INT PRIMARY KEY IDENTITY(1,1),
   fecha_creacion_contrato DATE NOT NULL,
@@ -68,33 +68,30 @@ CREATE TABLE Contratos (
   FOREIGN KEY (id_estado) REFERENCES Estados (id_estado),
   FOREIGN KEY (dni) REFERENCES Usuarios (dni)
 );
-
+-- 
 CREATE TABLE Documentos (
-  nombredocumento VARCHAR(20) NOT NULL,
+  nombre_documento VARCHAR(20) NOT NULL,
   id_contrato INT NOT NULL,
   ruta_documento VARCHAR(50) NOT NULL,
   fecha_adjunto DATE NOT NULL,
   FOREIGN KEY (id_contrato) REFERENCES Contratos (id_contrato),
-  PRIMARY KEY (nombredocumento, id_contrato)
+  PRIMARY KEY (nombre_documento, id_contrato)
 );
-
+-- 
 CREATE TABLE ValidacionFacial (
-  id_validacion INT IDENTITY(1,1),
+  id_validacion INT IDENTITY(1,1) NOT NULL,
   dni CHAR(8) NOT NULL,
   id_estado INT NOT NULL,
+  id_contrato INT NOT NULL,
   FOREIGN KEY (dni) REFERENCES Clientes(dni),
   FOREIGN KEY (id_estado) REFERENCES Estados (id_estado),
-  PRIMARY KEY (id_validacion, dni, id_estado)
-);
-
-CREATE TABLE Firmas (
-  id_validacion INT NOT NULL,
-  dni CHAR(8) NOT NULL,
-  id_contrato INT NOT NULL,
-  id_estado INT NOT NULL,
-  FOREIGN KEY (id_validacion, dni, id_estado) REFERENCES ValidacionFacial(id_validacion, dni, id_estado),
   FOREIGN KEY (id_contrato) REFERENCES Contratos (id_contrato),
   PRIMARY KEY (id_validacion, dni, id_estado, id_contrato)
+);
+-- VERIFICAR DE AQUI EN ADELANTE
+CREATE TABLE DetallesImagen (
+  id_detalles_imagen INT PRIMARY KEY IDENTITY(1,1),
+  detalle_imagen VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE Imagenes (
@@ -102,9 +99,8 @@ CREATE TABLE Imagenes (
   id_validacion INT NOT NULL,
   imagen VARCHAR(15) NOT NULL,
   fecha_envio_imagen DATE NOT NULL,
-  detalles_imagen VARCHAR(50) NOT NULL,
-  dni INT NOT NULL,
-  id_estado INT NOT NULL,
-  FOREIGN KEY (id_validacion, dni, id_estado) REFERENCES ValidacionFacial (id_validacion, dni, id_estado),
-  PRIMARY KEY (id_imagen, id_validacion, dni, id_estado)
+  id_detalles_imagen INT NOT NULL,
+  FOREIGN KEY (id_detalles_imagen) REFERENCES DetallesImagen (id_detalles_imagen),
+  FOREIGN KEY (id_validacion) REFERENCES ValidacionFacial (id_validacion),
+  PRIMARY KEY (id_imagen, id_validacion)
 );
