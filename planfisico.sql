1774ddac49ad0bd446f15a4912770ac3f2b71928
aarontimana04 <112721918+aarontimana04@users.noreply.github.com>
2024-11-13 17:24:04 -0500

    Update planfisico.sql

--- planfisico.sql
+++ planfisico.sql
@@ -53,7 +53,7 @@ CREATE TABLE ClienteGrupo (
   dni CHAR(8) NOT NULL,
   id_grupo INT NOT NULL,
   fecha_creacion_grupo DATE NOT NULL,
-  estado BIT NOY NULL,
+  estado BIT NOT NULL,
   FOREIGN KEY (dni) REFERENCES Clientes (dni),
   FOREIGN KEY (id_grupo) REFERENCES Grupos (id_grupo),
   PRIMARY KEY (dni, id_grupo)
