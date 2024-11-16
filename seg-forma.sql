946c52c455a4d701f8d2fc2cf10e7bd60fb2cd16
aarontimana04 <112721918+aarontimana04@users.noreply.github.com>
2024-11-16 18:49:28 -0500

    Update seg-forma.sql

--- seg-forma.sql
+++ seg-forma.sql
@@ -1,12 +1,13 @@
+-- CONSULTA 1
 SELECT E.nombre_empresa, COUNT(COALESCE(U.dni, NULL)) AS cantidad_personas
 FROM Empresas E
 LEFT JOIN Usuarios U ON E.id_empresa = U.id_empresa
 GROUP BY E.nombre_empresa;
-
+-- CONSULTA 2
 SELECT nombre_estado, 
        (SELECT COUNT(*) FROM Contratos C WHERE C.id_estado = E.id_estado) AS cantidad_contratos
 FROM Estados E;
-
+-- CONSULTA 3
 SELECT nombre_persona, contratos
 FROM (
     SELECT P.nombre_persona, COUNT(C.id_contrato) AS contratos
@@ -16,7 +17,7 @@ FROM (
 ) AS subconsulta
 WHERE contratos > 1;
 
-
+-- CONSULTA 4
 SELECT nombre_empresa, roles_asignados
 FROM (
     SELECT E.nombre_empresa, COUNT(DISTINCT U.id_rol) AS roles_asignados
@@ -26,30 +27,28 @@ FROM (
 ) AS subconsulta
 WHERE roles_asignados > 1;
 
-
+-- CONSULTA 5
 SELECT G.nombre_grupo, COUNT(CG.dni) AS cantidad_clientes
 FROM Grupos G
 JOIN ClienteGrupo CG ON G.id_grupo = CG.id_grupo
 GROUP BY G.nombre_grupo
 HAVING COUNT(*) > 2;
 
-
+-- CONSULTA 6
 SELECT C.id_contrato, E.nombre_empresa, EST.nombre_estado, 
        (SELECT G.nombre_grupo FROM Grupos G JOIN ClienteGrupo CG ON G.id_grupo = CG.id_grupo WHERE CG.dni = C.dni) AS nombre_grupo
 FROM Contratos C
 JOIN Empresas E ON C.id_empresa = E.id_empresa
 JOIN Estados EST ON C.id_estado = EST.id_estado;
 
-
-
+-- CONSULTA 7
 SELECT D.nombre_documento, C.id_contrato, P.nombre_persona, E.nombre_empresa, EST.nombre_estado
 FROM Documentos D
 INNER JOIN Contratos C ON D.id_contrato = C.id_contrato
 INNER JOIN Personas P ON C.dni = P.dni
 INNER JOIN Empresas E ON C.id_empresa = E.id_empresa
 INNER JOIN Estados EST ON C.id_estado = EST.id_estado;
-
-
+-- CONSULTA 8
 SELECT P.nombre_persona, C.id_contrato, E.nombre_empresa, G.nombre_grupo
 FROM Personas P
 INNER JOIN Contratos C ON P.dni = C.dni
@@ -57,7 +56,7 @@ INNER JOIN Empresas E ON C.id_empresa = E.id_empresa
 INNER JOIN ClienteGrupo CG ON P.dni = CG.dni
 INNER JOIN Grupos G ON CG.id_grupo = G.id_grupo;
 
-
+-- CONSULTA 9
 WITH ContratosPorEmpresa AS (
     SELECT id_empresa, COUNT(id_contrato) AS contratos_por_empresa
     FROM Contratos
@@ -67,7 +66,7 @@ SELECT E.nombre_empresa, AVG(CPE.contratos_por_empresa) AS promedio_contratos
 FROM Empresas E
 JOIN ContratosPorEmpresa CPE ON E.id_empresa = CPE.id_empresa
 GROUP BY E.nombre_empresa;
-
+-- CONSULTA 10
 SELECT EST.nombre_estado, 
        (SELECT COUNT(*) FROM Contratos C WHERE C.id_estado = EST.id_estado) AS total_contratos,
        (SELECT COUNT(DISTINCT C.dni) FROM Contratos C WHERE C.id_estado = EST.id_estado) AS cantidad_clientes
